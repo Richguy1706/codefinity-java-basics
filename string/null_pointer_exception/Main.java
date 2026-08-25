e9f9d81f57aa4a752ea146150dd764f9dcf28969
Richguy1706 <>
2026-08-25 17:41:51 +0000

    Null Pointer Exception

---
+++ string/null_pointer_exception/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Main {
+    public static void validateEmails(String[] emails) {
+        for (String email : emails ) {
+            
+            if ( email != null &&  email.contains("@")) {
+                System.out.println(emails);
+            } else {
+                System.out.println("Invalid or missing email");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] employeeEmails = {"alice@company.com", null, "bob@company.com", null, "charlie@company.com"};
+
+        validateEmails(employeeEmails);
+    }
+}
